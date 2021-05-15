2df034a7682ee2b7e8ee8e18a92f87c592958cc6
Rubber-Duck-999 <simon.crowther95@hotmail.co.uk>
2021-05-15 21:24:07 +0100

    Update shell

--- run.sh
+++ run.sh
@@ -11,4 +11,9 @@ fi
 
 export server_address="http://192.168.0.38:5000/weather"
 
-python3 /home/pi/Documents/weather/main.py & 2&>1 ../runtime.log
+if [ ! -f /home/pi/Documents/runtime.log ];
+then
+    touch /home/pi/Documents/runtime.log
+fi
+
+python3 /home/pi/Documents/weather/main.py & 2&>1 /home/pi/Documents//runtime.log
